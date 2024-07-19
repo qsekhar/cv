02748e4f684f09b8fe2c31105aed031238ef00d5
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2024-07-19 18:58:12 +0530

    checking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,8 @@
 import type { Metadata } from "next";
-
-import { Cormorant } from 'next/font/google'
-
+import { Inter } from "next/font/google";
 import "./globals.css";
 
-const cormorant = Cormorant({
-  weight: '400',
-  subsets: ['latin'],
-});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -21,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={cormorant.className}>{children}</body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
