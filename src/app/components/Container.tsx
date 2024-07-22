02949fd0e0595e3e601bf1f2afa1916906ae6b30
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2024-07-22 17:25:15 +0530

    working on about us

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,3 +1,3 @@
 export default function Container({ children }: { children: React.ReactNode }) {
-    return <div className="container mx-2 sm:mx-auto my-2 sm:my-10 p-4 border border-solid border-secondary border-4 border-dashed rounded-3xl">{children}</div>;
+    return <div className="container bg-background mx-2 sm:mx-auto my-2 sm:my-10 p-4 border border-solid border-primary border-2 border-dashed rounded-xl">{children}</div>;
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import dynamic from 'next/dynamic';
-import { Cormorant } from 'next/font/google'
+import { Stylish } from 'next/font/google'
+import { HiOutlinePhone, HiOutlineMail } from "react-icons/hi";
 
 const MoveingParticles = dynamic(() => import('./components/MoveingParticles'));
 const Container = dynamic(() => import('./components/Container'));
 const TimeLine = dynamic(() => import('./components/TimeLine'));
 
-const cormorant = Cormorant({
+const stylish = Stylish({
   weight: '400',
   subsets: ['latin'],
 });
@@ -15,15 +16,50 @@ export default function Home() {
   return (
     <main className="flex min-h-screen">
       <MoveingParticles  />
-      <Container>
-        <div className='flex justify-between'>
-          <div className={cormorant.className}>
-            <h1 className='text-4xl sm:text-9xl md:text-9xl'>Subhra Sekhar Mukherjee</h1>
+
+        <Container>
+          <div className={stylish.className}>
+            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+              <div>
+                <h2>Full Stack Developer</h2>
+              </div>
+              <div className='flex justify-center'>
+                <h3>Since 2012</h3>
+              </div>
+              <div>
+                <ul>
+                  <li><a href='tel:+91 9674540974' className='flex items-center gap-1'> <HiOutlinePhone /> +91 9674540974</a></li>
+                  <li><a href='mailto:subhra.php@gmail.com' className='flex items-center gap-1'> <HiOutlineMail />subhra.php@gmail.com</a></li>
+                  <li></li>
+                </ul>
+              </div>
+              <div></div>
+              <div className='col-span-2'>
+                <h1>Subhra Sekhar</h1>
+              </div>
+              <div>
+                <h1>Mukherjee</h1>
+              </div>
+
+              <div></div>
+              <div></div>
+              <div></div>
+              <div className='col-span-2'>
+                <h3 className='my-4'>About Me</h3>
+                <span className='text-lg'>Results-oriented and seasoned Full Stack Developer with over <span className='dottedUnderLine'>12 years</span> of hands-on
+                                      experience in <span className='dottedUnderLine'>designing, developing</span>, and implementing innovative <span className='dottedUnderLine'>software solutions.</span> Adept
+                                      at collaborating with cross-functional teams to deliver high-quality products that meet and
+                                      exceed client expectations. Proven expertise in the entire software development life cycle,
+                                      from concept and design to testing and deployment. Possess a strong background in both 
+                                      <span className='dottedUnderLine'> front-end and back-end technologies</span>, with a keen ability to leverage a diverse skill set to
+                                      tackle complex challenges.
+                </span>
+              </div>
+              <div></div>
+            </div>
           </div>
-          <TimeLine />
-        </div>
-        
-      </Container>
+        </Container>
+
     </main>
   );
 }
