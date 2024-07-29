a4fe40c65e79f09f1c08c79c74f74f783f98a2e1
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2024-07-29 14:38:23 +0530

    Anime initial setup

--- src/app/components/animations/FadeInWhenVisible.tsx
+++ src/app/components/animations/FadeInWhenVisible.tsx
@@ -1,23 +1,24 @@
 'use client'
 
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 
 type FadeInWhenVisibleProps = {
   children: React.ReactNode;
   delay?: number; // Add the delay prop with an optional type
+  varients?: Variants 
 };
 
-export default function FadeInWhenVisible({ children, delay = 0 }: FadeInWhenVisibleProps) {
+export default function FadeInWhenVisible({ children, delay = 0, varients={
+    visible: { opacity: 1, scale: 1, y: 0 },
+    hidden: { opacity: 0, scale: 1, y: '-100%' },
+} }: FadeInWhenVisibleProps) {
     return (
         <motion.div
             initial="hidden"
             whileInView="visible"
             viewport={{ once: true }}
-            transition={{ duration: 0.4, delay: delay }}
-            variants={{
-                visible: { opacity: 1, scale: 1 },
-                hidden: { opacity: 0, scale: 2 },
-            }}
+            transition={{ duration: 0.8, delay: delay, ease: "easeOut" }}
+            variants={varients}
         >
             {children}
         </motion.div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,109 +1,190 @@
-import dynamic from 'next/dynamic';
-import { Stylish } from 'next/font/google'
+import dynamic from "next/dynamic";
+import { Stylish } from "next/font/google";
 import { HiOutlinePhone, HiOutlineMail } from "react-icons/hi";
 import { CiLinkedin } from "react-icons/ci";
 import { FaGithub, FaWhatsapp, FaDiscord } from "react-icons/fa6";
 import { GiClick } from "react-icons/gi";
-import FadeInWhenVisible from './components/animations/FadeInWhenVisible';
+import FadeInWhenVisible from "./components/animations/FadeInWhenVisible";
 
-const MoveingParticles = dynamic(() => import('./components/MoveingParticles'));
-const Container = dynamic(() => import('./components/Container'));
-const TimeLine = dynamic(() => import('./components/TimeLine'));
-const Skills = dynamic(() => import('./components/Skills'));
-const Tools = dynamic(() => import('./components/Tools'));
-const OtherSkills = dynamic(() => import('./components/OtherSkills'));
-const Projects = dynamic(() => import('./components/Projects'));
-const Testimonials = dynamic(() => import('./components/Testimonials'));
-const SayHi = dynamic(() => import('./components/SayHi'));
-
+const MoveingParticles = dynamic(() => import("./components/MoveingParticles"));
+const Container = dynamic(() => import("./components/Container"));
+const TimeLine = dynamic(() => import("./components/TimeLine"));
+const Skills = dynamic(() => import("./components/Skills"));
+const Tools = dynamic(() => import("./components/Tools"));
+const OtherSkills = dynamic(() => import("./components/OtherSkills"));
+const Projects = dynamic(() => import("./components/Projects"));
+const Testimonials = dynamic(() => import("./components/Testimonials"));
+const SayHi = dynamic(() => import("./components/SayHi"));
 
 const stylish = Stylish({
-  weight: '400',
-  subsets: ['latin'],
+    weight: "400",
+    subsets: ["latin"],
 });
 
-
 export default function Home() {
-  return (
-    <main className="flex min-h-screen">
-      <MoveingParticles  />
-        <Container>
-          <div className={stylish.className}>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
-              <div>
-                <FadeInWhenVisible delay={1}><h1>Full Stack Developer</h1></FadeInWhenVisible>
-              </div>
-              <div className='flex flex-col items-center justify-center'>
-                <FadeInWhenVisible delay={1.5}>
-                  <h3>Since 2012</h3>
-                  <a href="./SubhraSekharMukherjeeResume.pdf" download="SubhraSekharMukherjeeResume.pdf" className='underline decoration-primary decoration-dotted underline-offset-2'>Download CV</a>
-                </FadeInWhenVisible>
-              </div>
-              <div className='md:flex md:flex-col md:items-end'>
-                <FadeInWhenVisible delay={2}>
-                  <ul>
-                    <li><a href='tel:+91 9674540974' className='flex items-center gap-2'> <HiOutlinePhone size={20}/> +91 9674 540 974</a></li>
-                    <li><a href='mailto:subhra.php@gmail.com' className='flex items-center gap-2'> <HiOutlineMail size={20} />subhra.php@gmail.com</a></li>
-                    <li className='flex items-center gap-2 mt-1'>
-                      <a target='_blank' href="https://api.whatsapp.com/send?phone=919674540974"><FaWhatsapp size={20}  /></a>
-                      <a target='_blank' href='https://www.linkedin.com/in/subhra-sekhar-mukherjee'><CiLinkedin size={20}/></a>
-                      <a target='_blank' href='https://github.com/qsekhar'><FaGithub size={20}/></a>
-                      <a target='_blank' href='https://discordapp.com/users/trozan7550/'><FaDiscord size={20}/></a>
-                    </li>
-                  </ul>
-                </FadeInWhenVisible>
-              </div>
-              <div></div>
-              <div className='col-span-2'>
-                <FadeInWhenVisible delay={0}><span className='h1'>Subhra Sekhar</span></FadeInWhenVisible>
-              </div>
-              <div>
-                <FadeInWhenVisible delay={0.5}><span className='h1'>Mukherjee</span></FadeInWhenVisible>
-              </div>
+    return (
+        <main className="flex min-h-screen">
+            <MoveingParticles />
+            <Container>
+                <div className={stylish.className}>
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+                        <div>
+                            <FadeInWhenVisible
+                                delay={1}
+                            >
+                                <h1>Full Stack Developer</h1>
+                            </FadeInWhenVisible>
+                        </div>
+                        <div className="flex flex-col items-center justify-center">
+                            <FadeInWhenVisible delay={1} >
+                                <h3>Since 2012</h3>
+                                <a
+                                    href="./SubhraSekharMukherjeeResume.pdf"
+                                    download="SubhraSekharMukherjeeResume.pdf"
+                                    className="underline decoration-primary decoration-dotted underline-offset-2"
+                                >
+                                    Download CV
+                                </a>
+                            </FadeInWhenVisible>
+                        </div>
+                        <div className="md:flex md:flex-col md:items-end">
+                            <FadeInWhenVisible delay={1.2}>
+                                <ul>
+                                    <li>
+                                        <a
+                                            href="tel:+91 9674540974"
+                                            className="flex items-center gap-2"
+                                        >
+                                            {" "}
+                                            <HiOutlinePhone size={20} /> +91
+                                            9674 540 974
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a
+                                            href="mailto:subhra.php@gmail.com"
+                                            className="flex items-center gap-2"
+                                        >
+                                            {" "}
+                                            <HiOutlineMail size={20} />
+                                            subhra.php@gmail.com
+                                        </a>
+                                    </li>
+                                    <li className="flex items-center gap-2 mt-1">
+                                        <a
+                                            target="_blank"
+                                            href="https://api.whatsapp.com/send?phone=919674540974"
+                                        >
+                                            <FaWhatsapp size={20} />
+                                        </a>
+                                        <a
+                                            target="_blank"
+                                            href="https://www.linkedin.com/in/subhra-sekhar-mukherjee"
+                                        >
+                                            <CiLinkedin size={20} />
+                                        </a>
+                                        <a
+                                            target="_blank"
+                                            href="https://github.com/qsekhar"
+                                        >
+                                            <FaGithub size={20} />
+                                        </a>
+                                        <a
+                                            target="_blank"
+                                            href="https://discordapp.com/users/trozan7550/"
+                                        >
+                                            <FaDiscord size={20} />
+                                        </a>
+                                    </li>
+                                </ul>
+                            </FadeInWhenVisible>
+                        </div>
+                        <div></div>
+                        <div className="col-span-2">
+                            <FadeInWhenVisible delay={0} varients={
+                                    {
+                                        visible: { opacity: 1, scale: 1, x: 0 },
+                                        hidden: {
+                                            opacity: 0,
+                                            scale: 1,
+                                            x: "-100%",
+                                        },
+                                    }
+                                }>
+                                <span className="h1">Subhra Sekhar</span>
+                            </FadeInWhenVisible>
+                        </div>
+                        <div>
+                            <FadeInWhenVisible delay={0.5} varients={
+                                    {
+                                        visible: { opacity: 1, scale: 1, x: 0 },
+                                        hidden: {
+                                            opacity: 0,
+                                            scale: 1,
+                                            x: "100%",
+                                        },
+                                    }
+                                }>
+                                <span className="h1">Mukherjee</span>
+                            </FadeInWhenVisible>
+                        </div>
 
-              <div></div>
-              <div></div>
-              <div></div>
-              <div className='col-span-2'>
-                <FadeInWhenVisible delay={2.5}>
-                  <h3 className='my-4'>Summary</h3>
-                  <span className='text-lg'>Full Stack Developer with 12+ years in the game, creating and dropping innovative software solutions. I&lsquo;m all about teaming up with different squads to deliver fire products that blow clients away. I&lsquo;ve got mad skills in every part of the software development cycle, from brainstorming and design to testing and launching. I&lsquo;m solid in both front-end and back-end tech and love using my diverse skills to tackle tough challenges.
-                  </span>
-                </FadeInWhenVisible>
-              </div>
-              <div></div>
-              <div className='col-span-2'>
-                <h3 className='my-4'>A small selection of my work, enjoy!</h3>
-                <Projects />
-              </div>
-              <div className='col-span-2'>
-                <Skills />
-              </div>
+                        <div></div>
+                        <div></div>
+                        <div></div>
+                        <div className="col-span-2">
+                            <FadeInWhenVisible delay={1.5}>
+                                <h3 className="my-4">Summary</h3>
+                                <span className="text-lg">
+                                    Full Stack Developer with 12+ years in the
+                                    game, creating and dropping innovative
+                                    software solutions. I&lsquo;m all about
+                                    teaming up with different squads to deliver
+                                    fire products that blow clients away.
+                                    I&lsquo;ve got mad skills in every part of
+                                    the software development cycle, from
+                                    brainstorming and design to testing and
+                                    launching. I&lsquo;m solid in both front-end
+                                    and back-end tech and love using my diverse
+                                    skills to tackle tough challenges.
+                                </span>
+                            </FadeInWhenVisible>
+                        </div>
+                        <div></div>
+                        <div className="col-span-2">
+                            <h3 className="my-4">
+                                A small selection of my work, enjoy!
+                            </h3>
+                            <Projects />
+                        </div>
+                        <div className="col-span-2">
+                            <Skills />
+                        </div>
 
-              <div className='col-span-2 md:col-span-1 flex flex-col items-start align-middle justify-center'>
-                <h3 className='flex items-center'>In case you tap below <GiClick /></h3>
-                <OtherSkills />
-              </div>
+                        <div className="col-span-2 md:col-span-1 flex flex-col items-start align-middle justify-center">
+                            <h3 className="flex items-center">
+                                In case you tap below <GiClick />
+                            </h3>
+                            <OtherSkills />
+                        </div>
 
-              <div></div>
-              <div className='col-span-2'>
-                <TimeLine />
-              </div>
+                        <div></div>
+                        <div className="col-span-2">
+                            <TimeLine />
+                        </div>
 
-              {/* <div className='col-span-2 md:col-span-3'>
+                        {/* <div className='col-span-2 md:col-span-3'>
                 <Testimonials />
               </div> */}
 
-              <div className='col-span-2'>
-                <SayHi />
-              </div>
-              
-              <div></div>
+                        <div className="col-span-2">
+                            <SayHi />
+                        </div>
 
-            </div>
-          </div>
-        </Container>
-
-    </main>
-  );
+                        <div></div>
+                    </div>
+                </div>
+            </Container>
+        </main>
+    );
 }
