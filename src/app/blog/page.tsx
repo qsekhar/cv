8054ed1ac5dddd18655a5142044c6c3e40ca83a3
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2024-08-01 16:55:07 +0530

    now its ok

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,42 +1,37 @@
-import type { InferGetStaticPropsType, GetStaticProps } from 'next'
 import fs from 'fs/promises';
+import Link from 'next/link';
 
 type Slug = string;
 type Post = { slug: Slug }
 type Posts = Post[]
+const getBlogPostMetadata = async () : Promise<Posts> => {
+    const folder = "src/posts";
 
-export const getStaticProps = (async () => {
-  const markDownFolder = 'src/posts';
-
-  const files = await fs.readdir(markDownFolder);
-  const posts = await Promise.all(
-    files.map(async (fileName) => {
-      const slug = fileName.replace('.md', '');
-      return {
-        slug,
-      };
-    })
-  );
-  return {
-    props: {
-      posts
+    const isDirectory = await fs.stat(folder).then((stat) => stat.isDirectory());
+    if (!isDirectory) {
+        throw new Error(`Folder ${folder} does not exist or is not a directory`);
     }
-  };
-}) satisfies GetStaticProps<{
-  posts : Posts
-}> 
+    const files = await fs.readdir(folder);
+    const markdownPosts = files.filter((file) => file.endsWith(".md"));
+    // get the blog post slug from the markdown file name
+    return markdownPosts.map((file) => {
+        const slug = file.replace(".md", "");
+        return { slug };
+    })
+}
 
-
-
-export default function Blog({
-  posts
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  console.log(posts)
+export default async function Blog() {
+  const postMetadata : Posts = await getBlogPostMetadata();
+  const postLinks = postMetadata && postMetadata.map((post: Post) => (
+      <li key={post.slug}>
+          <Link href={`/blog/posts/${post.slug}`}>{post.slug}</Link>
+      </li>
+  ))
   return (
       <div>
           <h1>Blog</h1>
           <ul>
-
+              {postLinks}
           </ul>
       </div>
   )
