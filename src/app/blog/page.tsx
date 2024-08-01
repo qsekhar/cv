8129d193e5e63b1358692b941db4748c0a386663
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2024-08-01 16:46:01 +0530

    but unable to use getstatic props

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,7 +1,43 @@
-export default function Blog() {
-    return (
-        <div>
-            <h1>Blog</h1>
-        </div>
-    )
+import type { InferGetStaticPropsType, GetStaticProps } from 'next'
+import fs from 'fs/promises';
+
+type Slug = string;
+type Post = { slug: Slug }
+type Posts = Post[]
+
+export const getStaticProps = (async () => {
+  const markDownFolder = 'src/posts';
+
+  const files = await fs.readdir(markDownFolder);
+  const posts = await Promise.all(
+    files.map(async (fileName) => {
+      const slug = fileName.replace('.md', '');
+      return {
+        slug,
+      };
+    })
+  );
+  return {
+    props: {
+      posts
+    }
+  };
+}) satisfies GetStaticProps<{
+  posts : Posts
+}> 
+
+
+
+export default function Blog({
+  posts
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  console.log(posts)
+  return (
+      <div>
+          <h1>Blog</h1>
+          <ul>
+
+          </ul>
+      </div>
+  )
 }
