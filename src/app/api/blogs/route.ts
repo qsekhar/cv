32cbd49a62480f5b3a9d0cfaca0d64678c95ee05
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2024-08-01 13:03:48 +0530

    initial setup

---
+++ src/app/api/blogs/route.ts
@@ -0,0 +1,9 @@
+export async function GET() {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const data = await response.json();
+    return new Response(JSON.stringify(data), {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    });
+}
