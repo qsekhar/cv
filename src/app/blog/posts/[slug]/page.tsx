5b9966de90e102878c33d20a2af615281a304eb7
Subhra Sekhar Mukherjee <subhra.php@gmail.com>
2024-08-02 19:07:16 +0530

    add links

--- src/app/blog/posts/[slug]/page.tsx
+++ src/app/blog/posts/[slug]/page.tsx
@@ -3,22 +3,21 @@ import fs from 'fs/promises'
 import Markdown from 'markdown-to-jsx'
 import { notFound } from 'next/navigation';
 import GetBlogPostMetadata from '../../../components/utils/GetBlogPostMetadata'
-import { Metadata, Slug } from "../../../components/interfaces/Post";
+import { Metadata } from "../../../components/interfaces/Post";
+import Link from "next/link";
+import { HiOutlineHome } from "react-icons/hi";
+import { RiArrowRightSLine } from "react-icons/ri";
+import { LiaBlogSolid } from "react-icons/lia";
 
 interface Props {
    params: {
-       slug: Slug
+       slug: string
    }
 }
 
 const folder : string = process.env.POST_FOLDER || ''
 
-if (!folder) {
-  throw new Error('POST_FOLDER environment variable is not defined.');
-}
-
-
-const getPostContent = async (slug: Slug) => {
+const getPostContent = async (slug: string) => {
   const file = `${folder}/${slug}.md`
 
   try {
@@ -40,7 +39,22 @@ const Post: NextPage<Props> = async (props: Props) => {
   const { slug } = props.params  
   const content = await getPostContent(slug)
   //
-  return  <article className="prose dark:prose-invert lg:prose-xl w-full"><Markdown key={slug} className="w-full">{content}</Markdown></article>
+  return  (
+    <article className="prose dark:prose-invert lg:prose-xl w-full">
+      <div className="flex items-center gap-2 smallBorderButtom">
+          <Link href="/" className="flex gap-2 text-primary items-center justify-start align-middle">
+              <HiOutlineHome size={20} />
+          </Link>
+
+          <RiArrowRightSLine size={20} />
+          
+          <Link href="/blog" className="flex gap-2 text-primary">
+            <LiaBlogSolid size={20} />
+          </Link>
+      </div>
+      <Markdown key={slug} className="w-full mt-4">{content}</Markdown>
+    </article>
+  )  
 }
 
 export default Post
